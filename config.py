264ccbdf891ff36f153c38942036b58881efe463
Paul Stanik <paulstanik3@gmail.com>
2022-02-07 15:27:26 -0800

    added and modified files for training on railsem19 dataset

--- config.py
+++ config.py
@@ -49,7 +49,8 @@ cfg = __C
 __C.GLOBAL_RANK = 0
 __C.EPOCH = 0
 # Absolute path to a location to keep some large files, not in this dir.
-__C.ASSETS_PATH = '/home/dcg-adlr-atao-data.cosmos277/assets'
+# __C.ASSETS_PATH = '/home/paul'
+__C.ASSETS_PATH = '/home/stanik'
 
 # Use class weighted loss per batch to increase loss for low pixel count classes per batch
 __C.BATCH_WEIGHTING = False
@@ -74,13 +75,17 @@ __C.TRAIN.FP16 = False
 
 #Attribute Dictionary for Dataset
 __C.DATASET = AttrDict()
+
+__C.DATASET.RAILSEM19 = \
+  os.path.join(__C.ASSETS_PATH, 'rtis_lab/data/RailSem19/custom_split')
+
 #Cityscapes Dir Location
 __C.DATASET.CITYSCAPES_DIR = \
-  os.path.join(__C.ASSETS_PATH, 'data/Cityscapes')
+  os.path.join(__C.ASSETS_PATH, 'rtis_lab/data/Cityscapes')
 __C.DATASET.CITYSCAPES_CUSTOMCOARSE = \
-  os.path.join(__C.ASSETS_PATH, 'data/Cityscapes/autolabelled')
+  os.path.join(__C.ASSETS_PATH, 'rtis_lab/data/Cityscapes/autolabelled')
 __C.DATASET.CENTROID_ROOT = \
-  os.path.join(__C.ASSETS_PATH, 'uniform_centroids')
+  os.path.join(__C.ASSETS_PATH, 'repos/nvidia_hrnet-ocr/uniform_centroids')
 #SDC Augmented Cityscapes Dir Location
 __C.DATASET.CITYSCAPES_AUG_DIR = ''
 #Mapillary Dataset Dir Location
@@ -137,7 +142,7 @@ __C.MODEL.MSCALE_INIT = 0.5
 __C.MODEL.ATTNSCALE_BN_HEAD = False
 __C.MODEL.GRAD_CKPT = False
 
-WEIGHTS_PATH = os.path.join(__C.ASSETS_PATH, 'seg_weights')
+WEIGHTS_PATH = os.path.join(__C.ASSETS_PATH, 'rtis_lab/data/Cityscapes/large_asset_dir/seg_weights')
 __C.MODEL.WRN38_CHECKPOINT = \
     os.path.join(WEIGHTS_PATH, 'wider_resnet38.pth.tar')
 __C.MODEL.WRN41_CHECKPOINT = \
